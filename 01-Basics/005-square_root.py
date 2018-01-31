2be50d7a2975036346dff5000013b0102dda06fd
Muhammad Javed <mjavedahmed@live.com>
2018-01-31 22:44:02 +0500

    Find the square root

---
+++ 01-Basics/005-square_root.py
@@ -0,0 +1,10 @@
+# Program to find the Square root
+
+num = 5
+
+num_sqrt = num ** 2
+
+print("Square root of ",num, " is ", num_sqrt)
+
+# In decimal points
+# print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
