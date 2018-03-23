f01262c3e513892cb06783e5d934772d49b9b35e
Muhammad Javed <mjavedahmed@live.com>
2018-03-23 19:21:36 +0500

    Python Strings

---
+++ 01-Basics/014-strings.py
@@ -0,0 +1,21 @@
+# Strings are not mutable! (meaning you can't use indexing to change individual elements of a string)
+# Just uncomment and check the single line example
+
+string = "Hello World"
+# string = 'Hello World'
+# string = ' "Hello World" '
+# string = " Hello I'm Javed "
+# string = string[0]
+# string = string[:4] # Grab First four or after 0 index
+# string = string[4:] # Grab after four indexes
+# string = string[::1] # Grab all which starts from 0
+# string = string[::-1] # Grab all from -1 or reverse it
+# string = "Hello \n \t World" # \n for newline \t for tab
+# string = string + ' Concatenate ' # Concatenate string
+# letter = 'z'
+# letter = letter * 10 # Multiply letter 10 times
+# string = string.upper() # String to upper case
+# string = string.lower() # String to lower case
+string = string.split() # Break string into list
+
+print(string)
