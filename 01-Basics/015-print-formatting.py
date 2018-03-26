d32dcc4576a2767dee008644a25aab7e31434e3f
Muhammad Javed <mjavedahmed@live.com>
2018-03-26 19:37:35 +0500

    Python - Print Formatting

---
+++ 01-Basics/015-print-formatting.py
@@ -0,0 +1,18 @@
+#!C:/python/python
+print("Content-type: text/html\n\n")
+
+s = "string"
+x = 123
+# print ("Place my variable here: %s" %s) # Prints the string and also convert this into string
+
+# print("Floating point number: %0.3f" %1335) # Prints the following floating number .3 is decimal point
+
+# print("Convert into string %r" %x) # %r / %s convert into string
+
+# print("First: %s Second: %s Third: %s" %('Hi','Hello',3))
+
+print("First: {x} Second: {y} Third: {x}" .format(x='Interested', y= 'Hi'))
+
+
+
+
