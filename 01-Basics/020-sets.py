acebbd6648c04d08d7490e82413d9aa17938af7b
javedbaloch4 <mjavedahmed@live.com>
2018-04-03 23:49:30 +0500

    Python - Sets & Booleans

---
+++ 01-Basics/020-sets.py
@@ -0,0 +1,12 @@
+
+x = set() # Create set
+
+x.add(1) # Adding Elements to set
+x.add(2)
+x.add(1)
+
+l = [1,1,1,2,2,3,3,4,4,5,5,6,6] # Creating list
+
+list_set = set(l) # Set will print only unique elements in list
+
+print(list_set)
---
+++ 01-Basics/021-booleans.py
@@ -0,0 +1,11 @@
+t = True # Creating booleans
+f = False
+
+a = 1 > 2
+
+b = 1 == 1
+
+c = None # Create but not assign it for now
+c = 'a'
+
+print(c)
